9b1386c48daed9857317dce3045411dda2a31733
Ashen-Design <ashen@ashendesign.co.uk>
2019-03-08 19:18:03 +0000

    Added Vault as dependency for permissions management.

--- src/main/java/uk/co/ashendesign/discordwhitelist/Main.java
+++ src/main/java/uk/co/ashendesign/discordwhitelist/Main.java
@@ -3,16 +3,24 @@ package uk.co.ashendesign.discordwhitelist;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
+
+//Import Vault permissions management
+import net.milkbowl.vault.permission.Permission;
 
 public class Main extends JavaPlugin{
 
     private File configFile;
     private FileConfiguration customConfig;
 
+    private static final Logger log = Logger.getLogger("Minecraft");
+
+    public static Permission perms = null;
 
     private static Main instance;
     public static Main getInstance() { return instance; }
@@ -32,11 +40,25 @@ public class Main extends JavaPlugin{
         createCustomConfig();
 
         instance = this;
+
+        //Enable Permissions
+        if(!setupPermissions()) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
     }
 
     public FileConfiguration getCustomConfig(){
         return this.customConfig;
     }
+
 
     //Create the initial config file for use in the plugin.
     private void createCustomConfig(){
